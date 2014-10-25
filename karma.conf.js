555f90181ab5ff08ef8eea1f9cd315e61a5e28e3
John Shanley <john.michael.shanley@gmail.com>
2014-10-25 16:38:18 -0400

    make karma exit by default after running tests

--- karma.conf.js
+++ karma.conf.js
@@ -56,11 +56,12 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'Firefox'],
 
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
+    singleRun: true
+
   });
 };
