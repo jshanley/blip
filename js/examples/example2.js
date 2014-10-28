5f86338fa7c2b3e0abd386f49c1d101b88c570dd
John Shanley <john.michael.shanley@gmail.com>
2014-10-28 13:53:14 -0400

    update example 2

--- js/examples/example2.js
+++ js/examples/example2.js
@@ -1,42 +1,31 @@
 blip.sampleLoader()
   .samples({
-    'bass': 'sounds/bassdrum.wav',
-    'cymbal': 'sounds/cymbal.wav',
-    'guitar': 'sounds/guitar.wav'
+    'glass': 'wav/glass.wav',
+    'plastic': 'wav/plastic.wav',
+    'can': 'wav/cokecan.wav'
   })
   .done(loaded)
   .load();
 
 function loaded() {
 
-  // set base tempo var
-  var TEMPO = 160;
+  var glass = blip.clip().sample('glass');
+  var plastic = blip.clip().sample('plastic');
+  var can = blip.clip().sample('can');
 
-  // create clips
-  var bass = blip.clip().sample('bass');
-  var cymbal = blip.clip().sample('cymbal');
-  var guitar = blip.clip().sample('guitar');
-
-  /* ====================== LOOPS ====================== */
-  var bassLoop = blip.loop()
-    .tempo(TEMPO)
-    .data([1, 0, 1, 0, 0, 1, 1, 0])
+  var loop = blip.loop()
+    .tempo(410)
+    .data([0.2,0.3,0.4,0.5,0.75,0.4])
     .tick(function(t,d) {
-      if (d) bass.play(t, { rate: d, gain: 0.4 });
-    })
-
-  var cymbalLoop = blip.loop()
-    .tempo(TEMPO)
-    .data([0, 1, 1, 0, 1, 0, 1, 0])
-    .tick(function(t,d) {
-      if (d) cymbal.play(t, { rate: d, gain: 0.7 });
-    })
-
-  var guitarLoop = blip.loop()
-    .tempo(TEMPO)
-    .data([1/2, 1/3, 1, 1/4, 1/4, 1, 1])
-    .tick(function(t, d) {
-      if(blip.chance(3/5)) guitar.play(t, { rate: d, gain: 0.4 });
+      if (blip.chance(4/5)) glass.play(t, { rate: d, gain: 0.6 });
+      if (blip.chance(2/5)) plastic.play(t, { rate: blip.random(0.4, 0.6), gain: 0.8 });
+      if (blip.chance(1/5)) can.play(t, { rate: blip.random(0.4, 0.8) });
+      if (blip.chance(1/24)) {
+        var factor = blip.chance(0.5) ? 3/2 : 2/3;
+        this.data(this.data().map(function(d) {
+          return d * factor;
+        }));
+      }
     })
 
   /* click events */
@@ -51,9 +40,7 @@ function loaded() {
     $('.pause-button').hide();
     $('#example2-play').hide();
     $('#example2-pause').show();
-    bassLoop.start();
-    cymbalLoop.start()
-    guitarLoop.start()
+    loop.start();
     window.currentBlips = [bassLoop, cymbalLoop, guitarLoop];
   });
   document.getElementById('example2-pause').addEventListener('click', function() {
