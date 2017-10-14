25c802502a8bb7060d9c794f6b48b6fe34391499
John Shanley <john.michael.shanley@gmail.com>
2017-10-14 09:04:57 -0400

    Remove previous build tools

--- systemjs-config.js
+++
@@ -1,8 +0,0 @@
-System.config({
-  defaultJSExtensions: true,
-  map: {
-    'plugin-babel': 'node_modules/systemjs-plugin-babel/plugin-babel.js',
-    'systemjs-babel-build': 'node_modules/systemjs-plugin-babel/systemjs-babel-node.js'
-  },
-  transpiler: 'plugin-babel'
-});
